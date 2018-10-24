489e77d8f2633ab6eb9e3a157ca5f41358acd0c7
Michael Granger <ged@FaerieMUD.org>
2018-10-23 18:26:28 -0700

    Initial commit.

---
+++ lib/saltpack.rb
@@ -0,0 +1,15 @@
+# -*- ruby -*-
+# frozen_string_literal: true
+
+# Toplevel namespace
+module Saltpack
+
+	# Package version
+	VERSION = '0.0.1'
+
+	# Version control revision
+	REVISION = %q$Revision: e216e8bc10bb $
+
+
+end # module Saltpack
+
---
+++ spec/saltpack_spec.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env rspec -cfd
+#encoding: utf-8
+
+require_relative 'spec_helper'
+
+require 'rspec'
+require 'saltpack'
+
+describe Saltpack do
+
+	
+
+end
+
