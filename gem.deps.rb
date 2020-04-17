2ffcdf29f1835ff6d0928dd48d2a32f6730f689f
Michael Granger <ged@FaerieMUD.org>
2020-04-16 17:07:23 -0700

    Checkpoint commit

---
+++ gem.deps.rb
@@ -0,0 +1,13 @@
+source 'https://rubygems.org/'
+
+gem 'loggability', "~> 0.15"
+gem 'rbnacl', "~> 7.1"
+gem 'msgpack', "~> 1.3"
+
+group :development do
+	gem 'rake-deveiate', "~> 0.10"
+	gem 'simplecov', "~> 0.18"
+	gem 'rdoc-generator-fivefish', "~> 0.3"
+	gem 'rdoc', "~> 6.2"
+end
+
